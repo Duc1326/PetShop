678f72cfc35d538e149cf79cd7f5d097dfcf8afd
Your Name <you@example.com>
2022-01-15 18:53:48 +0700

    sua file tiep

--- app/src/main/java/com/example/ducluu/petshop/utils/Utils.java
+++ app/src/main/java/com/example/ducluu/petshop/utils/Utils.java
@@ -3,11 +3,9 @@ package com.example.ducluu.petshop.utils;
 
 public class Utils {
 
-<<<<<<< HEAD
-    public static final String BASE_URL="http://192.168.1.7/";
-=======
+
     public static final String BASE_URL="http://192.168.1.3/";
->>>>>>> 71ca6fe9377bef57d8e8ca24adc11c6a3a06067c
+
     public static String getId = null;
 
 
