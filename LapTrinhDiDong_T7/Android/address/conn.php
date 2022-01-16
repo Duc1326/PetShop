f6f00c682703f5825311284f2972430db83ff3bd
Your Name <you@example.com>
2022-01-16 15:18:15 +0700

    minh up lai

--- LapTrinhDiDong_T7/Android/address/conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    $conn = mysqli_connect('localhost', 'root', '', 'address');
-    //Nếu có lỗi xảy ra thì dừng đoạn mã và in ra thông báo lỗi.
-		if(mysqli_connect_errno()!==0)
-		{
-			die("Error: Could not connect to the database. An error ".mysqli_connect_error()." ocurred.");
-		}
-    mysqli_set_charset($conn,'utf8');
